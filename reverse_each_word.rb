5336aca3dec331448e32897c9118567064dbd346
Firas Chaabani <firaschaabani1@gmail.com>
2019-12-20 02:03:49 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word (array)
+array.each do
+  puts "Hello there, and how are you?"
+
+end
+def reverse_each_word (array)
+  array.collect do
+    puts "Hi again, just making sure it's reversed! "
+  end
